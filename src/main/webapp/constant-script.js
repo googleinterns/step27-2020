ede5ed8e6a088e9533061647218c1f5d6eab5708
Peter Wu <peterlingwu@gmail.com>
2020-06-25 13:10:37 -0400

    Add hero section and text to homepage and navbar

--- src/main/webapp/constant-script.js
+++ src/main/webapp/constant-script.js
@@ -11,3 +11,11 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+// This file contains all the constant scripts to be loaded on each page.
+
+// Initialize mobile sidenav when DOM content is fully loaded
+document.addEventListener("DOMContentLoaded", () => {
+  const elems = document.querySelectorAll(".sidenav");
+  const instances = M.Sidenav.init(elems, undefined);
+});
