a0ee1491960da05584c64f0d8b4cd6a3c7384809
mffabrigas <migfabrigas@gmail.com>
2020-06-29 21:10:11 +0000

    Move Maps API and place-suggestions.js script to top of my-trips.html

--- src/main/webapp/scripts/place-suggestions.js
+++ src/main/webapp/scripts/place-suggestions.js
@@ -1,28 +1,28 @@
-let service;
-let map;
+var map;
+var service;
 
-const MTV = new google.maps.LatLng(37.4220, 122.0841);
+const MTV = new google.maps.LatLng(37.3861,122.0839);
 
 function findNearbyPlaces() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: MTV,
-        zoom: 15
+  map = new google.maps.Map(document.getElementById('map'), {
+      center: MTV,
+      zoom: 15
     });
 
-    let request = {
-        location: MTV,
-        radius: '5000',
-        type: ['restaurant']
-    };
+  let request = {
+    location: MTV,
+    radius: '5000',
+    type: ['restaurant']
+  };
 
-    service = new google.maps.places.PlacesService(map);
-    service.nearbySearch(request, callback);
+  service = new google.maps.places.PlacesService(map);
+  service.nearbySearch(request, callback);
 }
 
 function callback(results, status) {
-    if(status == google.maps.places.PlacesService.OK) {
-        for(let i = 0; i < 5; i++) {
-            console.log(results[i].name);
-        }
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      console.log(results[i].name);
     }
+  }
 }
