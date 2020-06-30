9666181366a73225e0fc3bc858ff44b3e992f250
migfabrigas <migfabrigas@gmail.com>
2020-06-30 13:37:23 -0500

    Add servlet to handle user authentication

---
+++ src/main/java/com/google/sps/servlets/AuthServlet.java
@@ -0,0 +1,36 @@
+package com.google.sps.servlets;
+
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import com.google.gson.Gson;
+import com.google.sps.data.AuthInfo;
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+public class AuthServlet extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    response.setContentType("application/json");
+    Gson gson = new Gson();
+
+    UserService userService = UserServiceFactory.getUserService();
+    if (userService.isUserLoggedIn()) {
+      String redirectAfterLogout = "/";
+      String logoutURL = userService.createLogoutURL(redirectAfterLogout);
+
+      AuthInfo userLoginStatus = AuthInfo.createLoggedInInfo(logoutURL);
+      response.getWriter().println(gson.toJson(userLoginStatus));
+    } else {
+      String redirectAfterLogin = "/";
+      String loginURL = userService.createLoginURL(redirectAfterLogin);
+
+      AuthInfo userLoginStatus = AuthInfo.createLoggedOutInfo(loginURL);
+      response.getWriter().println(gson.toJson(userLoginStatus));
+    }
+  }
+}
