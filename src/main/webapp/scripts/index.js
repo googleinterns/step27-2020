9b2e73b30ec93c3f0c8f9149cc76b33dc43ce8a6
mffabrigas <migfabrigas@gmail.com>
2020-06-30 18:28:02 -0500

    Add JSDoc for authUser

--- src/main/webapp/scripts/index.js
+++ src/main/webapp/scripts/index.js
@@ -1,10 +1,12 @@
+/**
+ * Check if user is logged in
+ */
 function authUser() {
     fetch('/login')
       .then(response => response.json())
       .then(userAuthInfo => {
         if(userAuthInfo.isLoggedIn) {
           console.log("User is logged in");
-          console.log("Please work git");
         } else {
           console.log("User is logged out");
         }
