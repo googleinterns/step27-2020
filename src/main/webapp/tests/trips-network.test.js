bf439758e10be02a05e775a528503d40bc9d77c0
Peter Wu <peterlingwu@gmail.com>
2020-07-01 16:29:04 +0000

    Add two more unit tests for unixTimestampToString

--- src/main/webapp/tests/trips-network.test.js
+++ src/main/webapp/tests/trips-network.test.js
@@ -1,5 +1,7 @@
 describe("Test unix timestamp toString converter", () => {
   it("converts timestamps correctly", () => {
     expect(unixTimestampToString(1593466864528)).toBe("6/29/2020");
+    expect(unixTimestampToString(1593620880101)).toBe("7/1/2020");
+    expect(unixTimestampToString(946702800000)).toBe("1/1/2000");
   });
 });
