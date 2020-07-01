312023a21652ae5d6ddb8a8ce5c0dcd3e5c68e69
mffabrigas <migfabrigas@gmail.com>
2020-07-01 12:22:30 -0500

    Make google login button functional with JS

--- src/main/webapp/scripts/index.js
+++ src/main/webapp/scripts/index.js
@@ -5,10 +5,15 @@ function authUser() {
     fetch('/login')
       .then(response => response.json())
       .then(userAuthInfo => {
+        let loginButton = document.getElementById('google-login-button');
+        let loginButtonText = document.getElementById('google-login-button-text');
+        
         if(userAuthInfo.isLoggedIn) {
-          console.log("User is logged in");
+          loginButton.setAttribute('href', userAuthInfo.logoutUrl);
+          loginButtonText.innerText('Sign out with Google');
         } else {
-          console.log("User is logged out");
+          loginButton.setAttribute('href', userAuthInfo.loginUrl);
+          loginButtonText.innerText('Sign in with Google');
         }
       });
 } 
