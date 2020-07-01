d632d7475a47bf0a66b2c9f41c3195d7d5a24c0f
mffabrigas <migfabrigas@gmail.com>
2020-07-01 12:46:00 -0500

    Fix improper setting of loginButtonText's innerText property

--- src/main/webapp/scripts/index.js
+++ src/main/webapp/scripts/index.js
@@ -7,13 +7,13 @@ function authUser() {
       .then(userAuthInfo => {
         let loginButton = document.getElementById('google-login-button');
         let loginButtonText = document.getElementById('google-login-button-text');
-        
+
         if(userAuthInfo.isLoggedIn) {
           loginButton.setAttribute('href', userAuthInfo.logoutUrl);
           loginButtonText.innerText('Sign out with Google');
         } else {
           loginButton.setAttribute('href', userAuthInfo.loginUrl);
-          loginButtonText.innerText('Sign in with Google');
+          loginButtonText.innerText = 'Sign in with Google';
         }
       });
 } 
