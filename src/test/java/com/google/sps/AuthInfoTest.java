3983639fc56eef211a32e08e3efd9f720be78742
Peter Wu <peterlingwu@gmail.com>
2020-07-02 00:50:25 +0000

    Add example tests to show build success

--- src/test/java/com/google/sps/AuthInfoTest.java
+++ src/test/java/com/google/sps/AuthInfoTest.java
@@ -7,8 +7,12 @@ import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
 public final class AuthInfoTest {
+  
   @Test
   public void example() {
+    // placeholder test to show that tests are evaluated
     Assert.assertEquals(1 + 1, 2);
+    Assert.assertEquals(2 * 2, 4);
+    Assert.assertEquals(1337, 1500 - 163);
   }
 }
