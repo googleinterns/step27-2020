da85016e6946181794c685ff69ebe863faf5ccc8
Peter Wu <peterlingwu@gmail.com>
2020-07-02 00:39:57 +0000

    Add JUnit to Maven build and placeholder unit test

---
+++ src/test/java/com/google/sps/AuthInfoTest.java
@@ -0,0 +1,14 @@
+package com.google.sps.tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public final class AuthInfoTest {
+  @Test
+  public void example() {
+    Assert.assertEquals(1 + 1, 2);
+  }
+}
