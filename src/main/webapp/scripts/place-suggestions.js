ea6104f1ddc86f2c0961c509335e884b73a576d3
mffabrigas <migfabrigas@gmail.com>
2020-07-13 19:48:47 +0000

    Implement city autocomplete + render place suggestions as cards to Plan For Me page

--- src/main/webapp/scripts/place-suggestions.js
+++ src/main/webapp/scripts/place-suggestions.js
@@ -1,42 +1,72 @@
 let map;
 let service;
 
-const MTV = new google.maps.LatLng(37.3861,122.0839);
-
 function init() {
   authReload();
   findNearbyPlaces();
 }
 /**
  * Inits map and calls a nearby search of prominent restaurants in 5000m 
- * radius of MTV
+ * radius of city inputted by user
  */
 function findNearbyPlaces() {
-  map = new google.maps.Map(document.getElementById('map'), {
-      center: MTV,
+  const cityInputField = document.getElementById('city-input');
+  const autocomplete = new google.maps.places.Autocomplete(cityInputField);
+  let userCity;
+
+  google.maps.event.addListener(autocomplete, 'place_changed', () => {
+    userCity = autocomplete.getPlace();
+    console.log(userCity);
+    const coords = {
+      lat: userCity.geometry.location.lat(),
+      lng: userCity.geometry.location.lng()
+    };
+
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: coords,
       zoom: 15
     });
 
-  let request = {
-    location: MTV,
-    radius: '5000',
-    type: ['restaurant']
-  };
+    let request = {
+      location: coords,
+      radius: '5000',
+      type: ['restaurant']
+    };
 
-  service = new google.maps.places.PlacesService(map);
-  service.nearbySearch(request, callback);
+    service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, renderPlaceCards);
+  });
 }
 
 /**
- * Logs the name of each place returned by nearby search in findNearbyPlaces()
- * to browser console
+ * Renders card containing place info to DOM
  * @param {Object} results array of PlaceResults objects
  * @param {Object} status status of Places API call
  */
-function callback(results, status) {
+function renderPlaceCards(results, status) {
   if (status === google.maps.places.PlacesServiceStatus.OK) {
-    for (let i = 0; i < results.length; i++) {
-      console.log(results[i].name);
+    const placeCardsContainer = document.getElementById('place-cards-container');
+    placeCardsContainer.innerHTML = '';
+
+    for(let i = 0; i < results.length; i++) {
+      console.log(results[i]);
     }
+
+    placeCardsContainer.innerHTML = results.map(
+      ({name, photos,rating}) => `
+        <div class="col s12 m6">
+          <div class="card">
+            <div class="card-image">
+              <img src="${photos[0].getUrl()}">
+              <span class="card-title">${name}</span>
+              <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+            </div>
+            <div class="card-content">
+              <p>Rating: ${rating}</p>
+            </div>
+          </div>
+        </div>
+      `
+    ).join("");
   }
 }
