afa52c717f7b14c54bf3ed837fd66d326d4ffdc2
Joshua Lewis <joshlewi14@gmail.com>
2020-07-14 14:07:31 -0500

    Add waypoint functionallity for multiple point routes

--- src/main/webapp/scripts/itinerary-overview.js
+++ src/main/webapp/scripts/itinerary-overview.js
@@ -1,16 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(elems, undefined);
+    const elems = document.querySelectorAll('select');
+    const instances = M.FormSelect.init(elems, undefined);
 });
 
 
 function initMap() {
-    var directionsRenderer = new google.maps.DirectionsRenderer();
-    var directionsService = new google.maps.DirectionsService();
+    const directionsRenderer = new google.maps.DirectionsRenderer();
+    const directionsService = new google.maps.DirectionsService();
 
-    const heath = new google.maps.LatLng(32.8368128, -96.4820992);
-
-    var map = new google.maps.Map(document.getElementById("map"), {
+    const map = new google.maps.Map(document.getElementById("map"), {
         zoom: 10,
         center: "25 Main Street, Belvedere Tiburon, CA"
     });
@@ -19,8 +17,13 @@ function initMap() {
     calculateAndDisplayRoute(directionsService, directionsRenderer);
     document.getElementById("mode").addEventListener("change", function() {
         calculateAndDisplayRoute(directionsService, directionsRenderer);
-        });
-    }
+    });
+
+    document.getElementById("waypoints").addEventListener("change", function() {
+        calculateAndDisplayRoute(directionsService, directionsRenderer);
+    });
+}
+
 
 /**
  * Takes a latitude and longitude pair as parameters and centers the map on that specific
@@ -46,14 +49,24 @@ function dropMarker(lat,lng) {
 }
 
 function calculateAndDisplayRoute(directionsService, directionsRenderer) {
-    const heath = new google.maps.LatLng(32.8368128, -96.4820992);
-    const summit = new google.maps.LatLng(33.0083762, -96.7793862);
+    var waypoints = [];
+    var checkboxArray = document.getElementById("waypoints");
+    for (var i = 0; i < checkboxArray.length; i++) {
+        if (checkboxArray.options[i].selected) {
+            waypoints.push({
+                location: checkboxArray[i].value,
+                stopover: true
+            });
+        }
+    }
 
-    var selectedMode = document.getElementById("mode").value;
+    const selectedMode = document.getElementById("mode").value;
     directionsService.route(
         {
             origin: "25 Main Street, Belvedere Tiburon, CA",
             destination: "Union Square, Post Street, San Francisco, CA",
+            waypoints: waypoints,
+            optimizeWaypoints: true,
             travelMode: google.maps.TravelMode[selectedMode]
         },
         function(response, status) {
@@ -66,31 +79,30 @@ function calculateAndDisplayRoute(directionsService, directionsRenderer) {
     );
 
     function markUserLocation() {
-    navigator.geolocation.getCurrentPosition(function(position) {
-        lat = position.coords.latitude;
-        lng = position.coords.longitude;
+        navigator.geolocation.getCurrentPosition(function(position) {
+            let lat = position.coords.latitude;
+            let lng = position.coords.longitude;
 
-        userPos = {
-            lat: lat,
-            lng: lng
-        };
+            let userPos = {
+                lat: lat,
+                lng: lng
+            };
 
-        setCenter(userPos);
-        dropMarker(lat,lng);
-    }, function() {
-        handleLocationError(true, infoWindow, map.getCenter());
-    });
+            setCenter(userPos);
+            dropMarker(lat,lng);
+        }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
 
     }
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(browserHasGeolocation ?
-        'Error: The Geolocation service failed.' :
-        'Error: Your browser doesn\'t support geolocation.');
-    infoWindow.open(map);
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+            'Error: The Geolocation service failed.' :
+            'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
 
     }
 
 }
-
