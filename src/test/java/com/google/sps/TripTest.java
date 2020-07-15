881b3f596405abef8da3ed45e5d6a1ab0435ba98
Peter Wu <peterlingwu@gmail.com>
2020-07-15 14:25:43 -0400

    Fix failing tests on backend

--- src/test/java/com/google/sps/TripTest.java
+++ src/test/java/com/google/sps/TripTest.java
@@ -13,11 +13,16 @@ public class TripTest {
   public void testDefaultBuilderValuesWithRequiredData() {
     Trip defaultTrip = Trip.builder()
                         .setTitle("Trip 1")
+                        .setHotelID("aBcXyZ123")
+                        .setHotelName("Default Hotel")
+                        .setHotelImage("https://google.com/aBcXyZ")
                         .setOwner("test@gmail.com")
                         .setTimestamp(1594160425000L)
                         .build();
     Assert.assertEquals("Trip 1", defaultTrip.title());
-    Assert.assertEquals("", defaultTrip.hotel());
+    Assert.assertEquals("aBcXyZ123", defaultTrip.hotelID());
+    Assert.assertEquals("Default Hotel", defaultTrip.hotelName());
+    Assert.assertEquals("https://google.com/aBcXyZ", defaultTrip.hotelImage());
     Assert.assertEquals(-1, defaultTrip.rating(), 0.01);
     Assert.assertEquals("", defaultTrip.description());
     Assert.assertEquals("test@gmail.com", defaultTrip.owner());
