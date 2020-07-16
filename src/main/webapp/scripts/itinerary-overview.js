b87b83b19e986d79c82e1b672dae4f8c99b88c52
Joshua Lewis <joshlewi14@gmail.com>
2020-07-16 14:19:29 -0500

    Test calculating and displaying routes through new functions

--- src/main/webapp/scripts/itinerary-overview.js
+++ src/main/webapp/scripts/itinerary-overview.js
@@ -1,3 +1,7 @@
+let start;
+let waypoints;
+let end;
+
 document.addEventListener('DOMContentLoaded', function() {
     const elems = document.querySelectorAll('select');
     const instances = M.FormSelect.init(elems, undefined);
@@ -11,20 +15,23 @@ function initMap() {
 
     const map = new google.maps.Map(document.getElementById("map"), {
         zoom: 10,
-        center: "25 Main Street, Belvedere Tiburon, CA"
+        center: start
     });
 
     transitLayer.setMap(map);
     directionsRenderer.setMap(map);
 
-    calculateAndDisplayRoute(directionsService, directionsRenderer);
+    calculateRoute()
+    displayRoute(directionsService, directionsRenderer,start,waypoints,end);
+
     document.getElementById("mode").addEventListener("change", function() {
-        calculateAndDisplayRoute(directionsService, directionsRenderer);
+        calculateRoute()
+        displayRoute(directionsService, directionsRenderer, start, waypoints, end);
     });
 
-    document.getElementById("waypoints").addEventListener("change", function() {
-        calculateAndDisplayRoute(directionsService, directionsRenderer);
-    });
+    // document.getElementById("waypoints").addEventListener("change", function() {
+    //     displayRoute(directionsService, directionsRenderer);
+    // });
 }
 
 
@@ -51,33 +58,86 @@ function dropMarker(lat,lng) {
     });
 }
 
-function calculateAndDisplayRoute(directionsService, directionsRenderer) {
-    const waypoints = [];
-    const checkboxArray = document.getElementById("waypoints");
-    for (let i = 0; i < checkboxArray.length; i++) {
-        if (checkboxArray.options[i].selected) {
-            waypoints.push({
-                location: checkboxArray[i].value,
-                stopover: true
-            });
+/**
+ * Takes the locations and hotel that was determined from the my trips class and organizes it into a start, midpoints
+ * which are placed into a Array in travel order and an end point.
+ */
+//Hard coded for test
+ function calculateRoute() {
+
+    start = "5902 N President George Bush Hwy, Garland, TX 75044, USA"
+
+    let rawWaypoints = ["525 Talbert Dr, Plano, TX 75093, USA",
+        "2134 Zurek Ln, Heath, TX 75126, USA",
+        "4234 Maple Ave #2403, Dallas, TX 75219, USA",]
+
+     waypoints = [];
+     end = "";
+
+     let length = rawWaypoints.length;
+
+    while(rawWaypoints.length > 1){
+        // console.log("In Loop")
+        for(let i = 0; i < rawWaypoints.length; i++){
+            if(waypoints.length !== (length - 1)){
+                // const response = await fetch(`https://maps.googleapis.com/maps/api/directions/json?' +
+                // 'origin=${hotel}' +
+                // '&destination=${rawMidpoints[i]}' +
+                // '&key=AIzaSyDlLtx69Y4-65_dCK67ZX3lzKTYpyc5CWI&output=json`)
+                console.log("Waypoint in If: " + rawWaypoints[0])
+                waypoints.push(rawWaypoints[0])
+                rawWaypoints.splice(0, 1)
+                // const {results} = await response.json();
+                // console.log(results)
+                }
+            }
         }
+    console.log("Last Point: " + rawWaypoints[0])
+    end += rawWaypoints.pop(0)
+
+    console.log("Start: " + start)
+    console.log("Waypoints: " + waypoints)
+    console.log("End: " + end)
+ }
+
+/**
+ * Takes the calculation done from the calculateRoutes function and display's the route onto the map
+ * @param directionsService - the google maps directions Service constructor "google.maps.DirectionsService()"
+ * @param directionsRenderer - the google maps directions Render constructor "google.maps.DirectionsRenderer()"
+ * @param waypoints - the points that are in between the start(hotel) and the end of the route in an Array (Strings)
+ * @param start - the origin of the trip which should be the hotel that the user is staying in.
+ * @param end - the "destination" or the last point in the users route.
+ */
+function displayRoute(directionsService, directionsRenderer, start, waypoints, end) {
+    // console.log("Starting to Display Route")
+
+    const waypointArray = [];
+    // const checkboxArray = document.getElementById("waypoints");
+    // for (let i = 0; i < checkboxArray.length; i++) {
+    //     if (checkboxArray.options[i].selected) {
+    //         waypointArray.push({
+    //             location: checkboxArray[i].value,
+    //             stopover: true
+    //         });
+    //     }
+    // }
+
+    for(let i = 0; i < waypoints.length; i++){
+        console.log("Dis Waypoint: " + waypoints[i])
+        waypointArray.push({
+            location: waypoints[i],
+            stopover: true
+        })
     }
 
-    // let locations = ["Steiner St & Hayes St, San Francisco, CA 94117", "Fisherman's Wharf, San Francisco, CA"]
-
-    // for(let i = 0; i < locations.length; i++){
-    //     waypoints.push({
-    //         location: locations[i],
-    //         stopover: true
-    //     })
-    // }
+    console.log("Done making waypoints locations")
 
     const selectedMode = document.getElementById("mode").value;
     directionsService.route(
         {
-            origin: "25 Main Street, Belvedere Tiburon, CA",
-            destination: "Union Square, Post Street, San Francisco, CA",
-            waypoints: waypoints,
+            origin: start,  //"25 Main Street, Belvedere Tiburon, CA",
+            destination: end, //"Union Square, Post Street, San Francisco, CA",
+            waypoints: waypointArray,
             optimizeWaypoints: true,
             travelMode: google.maps.TravelMode[selectedMode]
         },
