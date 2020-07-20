839c931a10ac69ac16fb6bad286e9a7071c5f1aa
mffabrigas <migfabrigas@gmail.com>
2020-07-20 21:26:49 +0000

    Change  user flow to ask for filter before city

--- src/main/webapp/scripts/place-suggestions.js
+++ src/main/webapp/scripts/place-suggestions.js
@@ -1,46 +1,50 @@
+const CITY_INPUT_FORM = `
+  <div id="city-input-area" class="row">
+    <h5 id="city-input-title" class="col s12 m7">
+      What city do you want to visit?
+    </h5>
+    <div id="city-input-field" class="input-field col s12 m5">
+      <input placeholder="Enter a city" id="city-input" type="text">
+    </div>
+  </div>
+`
+
 function init() {
   authReload();
+  addFilterListener();
 }
 
-/**
- * Inits map and calls a nearby search of prominent restaurants in 5000m 
- * radius of city inputted by user
- */
-function findNearbyPlaces() {
-  const filtersElem = document.getElementById("filters");
-  const filters = M.FormSelect.getInstance(filtersElem).getSelectedValues();
-  console.log(filters);
+function addFilterListener() {
+  const filterElem = document.getElementById('filter');
+  filterElem.addEventListener('change', () => {
+    const filter = filterElem.value;
+    addCityAutocomplete(filter);
+  });
 }
 
-/**
- * Renders card containing place info to DOM
- * @param {Object} results array of PlaceResults objects
- * @param {Object} status status of Places API call
- */
-function renderPlaceCards(results, status) {
-  if (status === google.maps.places.PlacesServiceStatus.OK) {
-    const placeCardsContainer = document.getElementById('place-cards-container');
-    placeCardsContainer.innerHTML = '';
-
-    for(let i = 0; i < results.length; i++) {
-      console.log(results[i]);
-    }
-
-    placeCardsContainer.innerHTML = results.map(
-      ({name, photos,rating}) => `
-        <div class="col s12 m6">
-          <div class="card">
-            <div class="card-image">
-              <img src="${photos[0].getUrl()}">
-              <span class="card-title">${name}</span>
-              <a class="btn-floating halfway-fab waves-effect waves-light blue"><i class="material-icons">add</i></a>
-            </div>
-            <div class="card-content">
-              <p>Rating: ${rating}</p>
-            </div>
-          </div>
-        </div>
-      `
-    ).join("");
+function addCityAutocomplete(filter) {
+  const inputElem = document.getElementById('input-container');
+  const cityInputElem = inputElem.querySelector('#city-input-area');
+  if(cityInputElem !== null) {
+    cityInputElem.innerHTML = '';
+    cityInputElem.remove();
   }
+  inputElem.insertAdjacentHTML('beforeend', CITY_INPUT_FORM);
+
+  const cityInputField = document.getElementById('city-input')
+  const autocomplete = new google.maps.places.Autocomplete(cityInputField);
+  google.maps.event.addListener(autocomplete, 'place_changed', () => {
+    city = autocomplete.getPlace();
+    findNearbyPlaces(city, filter);
+  });
 }
+
+function findNearbyPlaces(city, filter) {
+  console.log(city);
+  console.log(filter);
+} 
+
+
+
+
+
