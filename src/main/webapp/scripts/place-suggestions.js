739ce200280c41bdf218497739541ca57de216ad
mffabrigas <migfabrigas@gmail.com>
2020-07-21 16:26:30 +0000

    Implement two event listeners so user can input city or filter in any order

--- src/main/webapp/scripts/place-suggestions.js
+++ src/main/webapp/scripts/place-suggestions.js
@@ -1,49 +1,45 @@
-const CITY_INPUT_FORM = `
-  <div id="city-input-area" class="row">
-    <h5 id="city-input-title" class="col s12 m7">
-      What city do you want to visit?
-    </h5>
-    <div id="city-input-field" class="input-field col s12 m5">
-      <input placeholder="Enter a city" id="city-input" type="text">
-    </div>
-  </div>
-`
+let city;
+let filter;
 
 function init() {
   authReload();
-  addFilterListener();
+  addCityAutocomplete();
+  addFilterHandler();
 }
 
-function addFilterListener() {
-  const filterElem = document.getElementById('filter');
-  filterElem.addEventListener('change', () => {
-    const filter = filterElem.value;
-    addCityAutocomplete(filter);
-  });
-}
-
-function addCityAutocomplete(filter) {
-  const inputElem = document.getElementById('input-container');
-  const cityInputElem = inputElem.querySelector('#city-input-area');
-  if(cityInputElem !== null) {
-    cityInputElem.innerHTML = '';
-    cityInputElem.remove();
-  }
-  inputElem.insertAdjacentHTML('beforeend', CITY_INPUT_FORM);
-
+function addCityAutocomplete() {
   const cityInputField = document.getElementById('city-input')
   const autocomplete = new google.maps.places.Autocomplete(cityInputField);
   google.maps.event.addListener(autocomplete, 'place_changed', () => {
     city = autocomplete.getPlace();
-    findNearbyPlaces(city, filter);
+    if(typeof filter !== 'undefined') {
+      findPlacesInCity(city, filter);
+    }
   });
 }
 
-function findNearbyPlaces(city, filter) {
+function addFilterHandler() {
+  const filterForm = document.getElementById('filters');
+  filterForm.addEventListener('change', () => {
+    filter = filterForm.value;
+    if(typeof city !== 'undefined') {
+      findPlacesInCity(city, filter);
+    }
+  });
+}
+
+function findPlacesInCity(city, filter) {
+  if (city.hasOwnProperty('place_id') === false) {
+    M.Toast.dismissAll();
+    M.toast({
+      html: "Your city wasn't selected with autocomplete",
+    });
+    return;
+  }
+  
   console.log(city);
   console.log(filter);
 } 
-
 
 
 
