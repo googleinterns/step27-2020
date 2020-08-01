9cad7f5836ea9b98ef6d2ea44e2c3df71e9780e0
Peter Wu <peterlingwu@gmail.com>
2020-07-31 20:59:58 -0400

    Add test for unauthenticated user all 4 HTTP methods in TripsServlet

--- src/test/java/com/google/sps/TripsServletTest.java
+++ src/test/java/com/google/sps/TripsServletTest.java
@@ -1,5 +1,14 @@
 package com.google.sps;
 
+import static org.mockito.Mockito.*;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
@@ -16,5 +25,41 @@ import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
 public class TripsServletTest {
+  private LocalServiceTestHelper helper;
 
+  @Before
+  public void setUp() {
+    helper = new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig());
+  }
+
+  @After
+  public void tearDown() {
+    helper.tearDown();
+  }
+
+  @Test
+  public void testUnauthenticatedUser() throws IOException {
+    helper.setEnvIsLoggedIn(false);
+    helper.setUp();
+
+    HttpServletRequest request = mock(HttpServletRequest.class);
+    HttpServletResponse response = mock(HttpServletResponse.class);
+    PrintWriter pw = new PrintWriter(new StringWriter());
+    when(response.getWriter()).thenReturn(pw);
+
+    TripsServlet servlet = new TripsServlet();
+    servlet.doGet(request, response);
+    servlet.doPost(request, response);
+    servlet.doPut(request, response);
+    servlet.doDelete(request, response);
+    verify(response, times(4)).setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+  }
+
+  @Test
+  public void testDoGet() {
+    helper.setEnvIsLoggedIn(true);
+    helper.setEnvEmail("peter@google.com");
+    helper.setEnvAuthDomain("google.com");
+    helper.setUp();
+  }
 }
