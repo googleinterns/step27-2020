769598b5837a1698d7d9c7a498a29ba736044305
Peter Wu <peterlingwu@gmail.com>
2020-08-04 14:35:33 -0400

    Complete test coverage for doPut and helper function for testing

--- src/test/java/com/google/sps/TripsServletTest.java
+++ src/test/java/com/google/sps/TripsServletTest.java
@@ -11,13 +11,18 @@ import java.io.StringWriter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.dev.LocalDatastoreService;
 import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.sps.data.Trip;
+import com.google.sps.data.TripLocation;
 import com.google.sps.servlets.TripsServlet;
 import com.google.sps.util.TripDataConverter;
 
@@ -34,15 +39,18 @@ public class TripsServletTest {
   private HttpServletRequest request;
   private HttpServletResponse response;
   private PrintWriter pw;
+  private long timestamp = 1596490838000L;
+  private TripLocation tripLocation1;
+  private TripLocation tripLocation2;
   private JsonObject postRequestBody;
   private JsonObject putRequestBody;
   private TripsServlet servlet;
+  private DatastoreService datastore;
 
   @Before
   public void setUp() throws IOException {
     helper = new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig());
-    LocalDatastoreService dsService = (LocalDatastoreService) helper.getLocalService(LocalDatastoreService.PACKAGE);
-    dsService.setNoStorage(false);
+    datastore = DatastoreServiceFactory.getDatastoreService();
     request = mock(HttpServletRequest.class);
     response = mock(HttpServletResponse.class);
     servlet = new TripsServlet();
@@ -64,14 +72,17 @@ public class TripsServletTest {
     location2.addProperty("id", "c0mput3r");
     location2.addProperty("name", "Computer History Museum");
     location2.addProperty("weight", 3);
+    tripLocation1 = TripDataConverter.convertJsonObjectToTripLocation(location1, "peter@google.com");
+    tripLocation2 = TripDataConverter.convertJsonObjectToTripLocation(location2, "peter@google.com");
+
     JsonArray locationArray = new JsonArray();
     locationArray.add(location1);
     locationArray.add(location2);
     postRequestBody.add("locations", locationArray);
 
     putRequestBody = postRequestBody.deepCopy();
-    putRequestBody.addProperty("timestamp", 1596490838000L);
-
+    putRequestBody.addProperty("timestamp", timestamp);
+    putRequestBody.addProperty("title", "My Trip, Updated");
   }
 
   @After
@@ -101,16 +112,56 @@ public class TripsServletTest {
   }
 
   @Test
-  public void testDoPostAndPut() throws IOException {
+  public void testDoPost() throws IOException {
     setUserAuthenticated();
     when(request.getReader()).thenReturn(new BufferedReader(new StringReader(postRequestBody.toString())));
     servlet.doPost(request, response);
 
     verify(response).setStatus(HttpServletResponse.SC_CREATED);
+  }
+
+  @Test
+  public void testDoPutTripExists() throws IOException {
+    postPreexistingTrip();
+    Entity initTrip = datastore.prepare(new Query("trip")).asSingleEntity();
+    Assert.assertEquals(1, datastore.prepare(new Query("trip")).countEntities(FetchOptions.Builder.withLimit(10)));
+    Assert.assertEquals(2,
+        datastore.prepare(new Query("trip-location")).countEntities(FetchOptions.Builder.withLimit(10)));
+    Assert.assertEquals("My Trip", initTrip.getProperty("title"));
+
     when(request.getReader()).thenReturn(new BufferedReader(new StringReader(putRequestBody.toString())));
     servlet.doPut(request, response);
 
     verify(response).setStatus(HttpServletResponse.SC_OK);
+    Entity updatedTrip = datastore.prepare(new Query("trip")).asSingleEntity();
+    Assert.assertEquals(1, datastore.prepare(new Query("trip")).countEntities(FetchOptions.Builder.withLimit(10)));
+    Assert.assertEquals(2,
+        datastore.prepare(new Query("trip-location")).countEntities(FetchOptions.Builder.withLimit(10)));
+    Assert.assertEquals("My Trip, Updated", updatedTrip.getProperty("title"));
+
+  }
+
+  @Test
+  public void testDoPutTripDoesNotExist() throws IOException {
+    setUserAuthenticated();
+    when(request.getReader()).thenReturn(new BufferedReader(new StringReader(putRequestBody.toString())));
+    servlet.doPut(request, response);
+
+    verify(response).setStatus(HttpServletResponse.SC_NOT_FOUND);
+  }
+
+  /**
+   * Sets the user as authenticated and logged in as peter@google.com, and inserts
+   * the example trip modeled by postRequestBody to the Datastore.
+   */
+  public void postPreexistingTrip() {
+    setUserAuthenticated();
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    Trip trip = TripDataConverter.convertJsonObjectToTrip(postRequestBody, "peter@google.com", timestamp);
+    Entity tripEntity = TripDataConverter.convertTripToEntity(trip);
+    datastore.put(tripEntity);
+    datastore.put(TripDataConverter.convertTripLocationToEntity(tripLocation1, tripEntity));
+    datastore.put(TripDataConverter.convertTripLocationToEntity(tripLocation2, tripEntity));
   }
 
   /**
@@ -120,7 +171,7 @@ public class TripsServletTest {
   private void setUserAuthenticated() {
     helper.setEnvIsLoggedIn(true);
     helper.setEnvEmail("peter@google.com");
-    helper.setEnvAuthDomain("google.com");
+    helper.setEnvAuthDomain("localhost");
     helper.setUp();
   }
 }
